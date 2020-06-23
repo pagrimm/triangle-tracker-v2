be6b2476cf8a24ac56c6bb1fce9e96c4136a81da
Peter Grimm <pagrimm@gmail.com>
2020-06-23 09:58:33 -0700

    make test pass
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- __tests__/triangletracker.tests.js
+++ __tests__/triangletracker.tests.js
@@ -3,7 +3,7 @@ import { Triangle } from './../src/triangletracker.js';
 describe('Triangle', () => {
 
   test('should correctly create a triangle object with three lengths', () => {
-    const triangle = new Triangle();
+    const triangle = new Triangle(2, 4, 5);
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
